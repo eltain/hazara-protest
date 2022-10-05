3b1684c7a6ad7ef43d8eab65f510c10589c43d08
eltafhussain <watandosth@gmail.com>
2022-10-05 22:30:59 +1100

    footer fixes

--- components/Layout.js
+++ components/Layout.js
@@ -17,17 +17,8 @@ function Layout({ title = "Stop Hazara Genocide | Protest Map", description = "S
             </main>
 
             <footer className={styles.footer}>
-                To add your protest location, &nbsp; <a target="_blank" href="https://docs.google.com/forms/d/1n-53iQJLTQjCOcRdhl_2G1E_KNQ9X5rNt57jFsD9_2g/viewform?edit_requested=true"> Please fill the form here</a>
-                {/* <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a> */}
+                To add your protest location, &nbsp; <a target="_blank" rel="noreferrer" href="https://docs.google.com/forms/d/1n-53iQJLTQjCOcRdhl_2G1E_KNQ9X5rNt57jFsD9_2g/viewform?edit_requested=true"> Please fill the form here</a>
+               
             </footer>
         </div>
     )
